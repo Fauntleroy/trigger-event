b25574639d6fc6049993b630172a0c6d31f24d47
Timothy Kempf <the.kaempfer@gmail.com>
2015-09-27 12:32:50 -0700

    automatically test all supported events

--- src/getConstructorByName.js
+++ src/getConstructorByName.js
@@ -1,21 +1,25 @@
-var getConstructorByName = function (name) {
-  switch (name) {
-    case 'click':
-    case 'contextmenu':
-    case 'dblclick':
-    case 'mousedown':
-    case 'mouseenter':
-    case 'mouseleave':
-    case 'mousemove':
-    case 'mouseout':
-    case 'mouseover':
-    case 'show':
-      return MouseEvent;
-    break;
-    default:
-      return Event;
-    break;
-  }
+var eventsToTypesMap = {
+  click: 'mouse',
+  contextmenu: 'mouse',
+  dblclick: 'mouse',
+  mousedown: 'mouse',
+  mouseenter: 'mouse',
+  mouseleave: 'mouse',
+  mousemove: 'mouse',
+  mouseout: 'mouse',
+  mouseover: 'mouse',
+  show: 'mouse'
 };
 
+var typesToConstructorsMap = {
+  mouse: MouseEvent
+};
+
+var getConstructorByName = function (name) {
+  var type = eventsToTypesMap[name];
+  var Constructor = typesToConstructorsMap[type] || Event;
+  return Constructor;
+};
+
+export { eventsToTypesMap, typesToConstructorsMap };
 export default getConstructorByName;
--- test/triggerEvent.js
+++ test/triggerEvent.js
@@ -1,15 +1,32 @@
 import test from 'tape';
 import { mock } from 'simple-mock';
+import _ from 'lodash';
 
 import triggerEvent from '../src/triggerEvent';
+import { eventsToTypesMap } from '../src/getConstructorByName';
 
-test('Triggers a click event', function (t) {
-  t.plan(2);
-  var clickMock = mock();
-  document.addEventListener('click', clickMock);
-  triggerEvent(document, 'click', {
+var typesToElementsMap = {
+  mouse: document
+};
+var typesToPropertiesMap = {
+  mouse: {
     clientX: 50
+  }
+};
+
+_.forEach(eventsToTypesMap, function (eventType, eventName) {
+  var eventElement = typesToElementsMap[eventType];
+  var eventProperties = typesToPropertiesMap[eventType];
+
+  test(`Triggers the ${eventName} event`, function (t) {
+    var eventCallback = mock();
+    eventElement.addEventListener(eventName, eventCallback);
+    triggerEvent(eventElement, eventName, eventProperties);
+    t.ok(eventCallback.called, `${eventName} callback fired`);
+    _.forEach(eventProperties, (eventPropertyValue, eventPropertyName) => {
+      var eventCallbackObject = eventCallback.lastCall.args[0];
+      t.equal(eventCallbackObject[eventPropertyName], eventPropertyValue, `${eventName} callback called with provided ${eventPropertyName} property`);
+    });
+    t.end();
   });
-  t.ok(clickMock.called, 'Event callback fired');
-  t.equal(clickMock.lastCall.args[0].clientX, 50, 'Event callback called with provided propeties');
 });
